d9b0e7fac58bac646d873bb0ad4c3b760a59e99a
Julian Leon <jleon1992@gmail.com>
2020-09-16 00:25:05 -0400

    nothuing wiorks

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+// server.use('/api/auth', authRouter);
+// server.use('/api/jokes', authenticate, jokesRouter);
 
 module.exports = server;
